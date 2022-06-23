1c4a68c6a986e693a575b9ce1eac0a53017f9629
roku674 <af03848@georgiasouthern.edu>
2022-06-23 07:22:52 -0400

    Create Resources.cs

---
+++ Resources.cs
@@ -0,0 +1,28 @@
+﻿//Created by Alexander Fields https://github.com/roku674
+
+namespace StarportBuilderBot.Objects.StarportObjects
+{
+    public class Resources
+    {
+        public uint anerobes { get; set; }
+        public uint equipment { get; set; }
+        public uint medicine { get; set; }
+        public uint metal { get; set; }
+        public uint oil { get; set; }
+        public uint organics { get; set; }
+        public uint spice { get; set; }
+        public uint uranium { get; set; }
+
+        public Resources(uint metal, uint anerobes, uint medicine, uint organics, uint oil, uint uranium, uint equipment, uint spice)
+        {
+            this.metal = metal;
+            this.anerobes = anerobes;
+            this.medicine = medicine;
+            this.organics = organics;
+            this.oil = oil;
+            this.uranium = uranium;
+            this.equipment = equipment;
+            this.spice = spice;
+        }
+    }
+}
