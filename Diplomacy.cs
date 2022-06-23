f641b33d879e3aa529c612f7ff0e84235f4061ad
roku674 <af03848@georgiasouthern.edu>
2022-06-23 06:46:57 -0400

    Create Diplomacy.cs
    seperate into seperate object so i can import in both projects

---
+++ Diplomacy.cs
@@ -0,0 +1,27 @@
+//Created by Alexander Fields https://github.com/roku674
+
+namespace StarportDiplomacy
+{
+    public class Diplomacy
+    {
+        private static readonly string[] _allies =
+        {
+            "Allie", "Anxiety.jar","Bitcoin", "Bodhi", "CaptArcher",
+            "Depression.Wav", "Devila", "Hokujinn","Jumjumbub1410",
+            "Leaderkiller","Probation", "Taterchip","Towlie", "WW3"
+        };
+
+        private static readonly string[] _enemies =
+                {
+            "Altair","Awmalzo","Booga.Booga", "B-radk.","Dad","Darkside", "Demon",
+            "Deegs", "Flint","Juneteenth", "Meshuggah","McGee","Pluto","Presto",
+            "Revelation","RONXINA","Scar-Face"
+        };
+
+        private static readonly string[] _nap = { };
+
+        public static string[] allies => _allies;
+        public static string[] enemies => _enemies;
+        public static string[] nap => _nap;
+    }
+}
