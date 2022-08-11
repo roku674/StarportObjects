d0069cc98b4fc8186864b1db8a29adfdb47d23f4
roku674 <af03848@georgiasouthern.edu>
2022-08-11 12:19:55 -0400

    Update Diplomacy.cs

--- Diplomacy.cs
+++ Diplomacy.cs
@@ -7,17 +7,17 @@ namespace StarportObjects
         private static readonly string[] _allies =
         {
             "Allie","Anxiety.jar","BananaDei","Bitcoin","Bodhi","CaptArcher","ComputerSheep",
-            "Depression.Wav","Devila","Hokujinn","Jumjumbub1410",
-            "Leaderkiller","Muzza269u","MRniceguy","Probation","Rancia","Taterchip","Towlie","WW3"
+            "Depression.Wav","Devila","Fayil","Hokujinn","Jumjumbub1410",
+            "Leaderkiller","Muzza269u","MRniceguy","Probation","Rancia","Shmandoor","Taterchip","Towlie","WW3"
         };
 
-        private static readonly string[] _nap = { "Fayil", "Legion", "Pebo", "Ted" };
+        private static readonly string[] _enemies = {
             "Adapt","Altair","Ammimo","Awmalzo","Beansmasher","Booga.Booga","B-radk.","Dad","Darkside","Demon","Demon.",
             "Deegs","DrakeoTheRuler","Cockhead","Flint","Juneteenth","Meshuggah","McGee","PIRI-38-tinto","Pluto","Presto",
-            "Revelation"," Ron ","RON-4-GOLD","Scar-Face","Slaven"
+            "Revelation","River,"," Ron ","RON-4-GOLD","Scar-Face","Slaven"
         };
 
-        private static readonly string[] _nap = { "Fayil", "Legion", "Mega.Tron", "Pebo", "Shmandoor", "Ted" };
+        private static readonly string[] _nap = { "Legion", "Mega.Tron", "Pebo", "Ted" };
 
         public static string[] allies => _allies;
         public static string[] enemies => _enemies;
