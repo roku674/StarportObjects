ec09e13690f6222c601819696ef1c059cab0dd4c
roku674 <af03848@georgiasouthern.edu>
2022-09-02 18:59:08 -0400

    Update StarSystem.cs

--- StarSystem.cs
+++ StarSystem.cs
@@ -7,6 +7,7 @@ namespace StarportObjects
     public class StarSystem
     {
         public static readonly uint defensesMax = 60;
+
         public string name;
 
         /// <summary>
@@ -22,14 +23,16 @@ namespace StarportObjects
         /// <param name="currentDefenses"></param>
         /// <param name="planets"></param>
         /// <param name="port"></param>
-        public StarSystem(string name, uint currentDefenses, List<Planet> planets, Starport port)
+        public StarSystem(string name, uint currentDefenses, List<Planet> planets, Starport port, List<StarSystem> connectedSystems)
         {
             this.name = name;
             this.currentDefenses = currentDefenses;
             this.planets = planets;
             this.port = port;
+            this.connectedSystems = connectedSystems;
         }
 
+        public List<StarSystem> connectedSystems { get; set; }
         public uint currentDefenses { get; set; }
         public List<Planet> planets { get; set; }
         public Starport port { get; set; }
